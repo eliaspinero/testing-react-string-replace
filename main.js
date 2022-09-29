77f26d652f9aa743d8028a8b2c875eacf55534c9
eliaspinero <eliasrpt14@gmail.com>
2022-09-29 18:04:15 -0430

    preparedForLibrary

--- main.js
+++ main.js
@@ -1,21 +1,4 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
 import reactStringReplace from 'react-string-replace'
-
-const text = `adopción
-
-## Definición
-
-El término “adopción” se refiere al proceso de alguien legalmente convertirse en el hijo de personas que no son sus padres biológicos.
-
-* La Biblia usa “adopción” y “adoptar” en un sentido figurado para describir como Dios convierte a las personas en parte de su familia, haciéndolos sus hijos espirituales.
-* Como hijos adoptados, los creyentes son coherederos con Jesucristo, teniendo el derecho de todos los privilegios de los hijos de Dios.
-
-## Sugerencias de traducción
-
-* Este término puede ser traducido con un término que el lenguaje de la traducción utilice para describir esta relación especial entre padres e hijos. Asegúrese que sea entendido que esto tiene un significado figurado o espiritual.
-* La frase “experimentar la adopción como hijos” puede ser traducida como “ser adoptados por Dios como sus hijos” o “convertirse en Hijos de Dios”.`;
 
 function replace({baseText, searchText , replaceText = '', options = {}, replaceIndexes}){
 
@@ -50,28 +33,4 @@ function replace({baseText, searchText , replaceText = '', options = {}, replace
   return {replacedText, occurrences: getOccurrences(occurrencesSource)};
 };
 
-const auxiliar = replace({baseText:text, searchText:'adopción', replaceText: '!texto!', replaceIndexes: [0, 2], options:{beforeMatch: 'HHHHHHHH'}});
-console.log(auxiliar);
-
-console.log(auxiliar.replacedText);
-
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+export default replace;
